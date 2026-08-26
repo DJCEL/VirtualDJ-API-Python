f18841ac9111c934cc4649e0980ba9270ef931af
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-08-26 08:22:29 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import sys
-import asyncio
 from rich.console import Console
 
 from client import VirtualDJClient
@@ -16,30 +15,34 @@ def main():
     client = VirtualDJClient()
 
     client_connected = False
-    client_connected = asyncio.run(client.is_running())
+    client_connected = client.run("is_running")
     console.print(f"VirtualDJ connected: {client_connected}")
     if (client_connected == False):
         sys.exit()
 
-    vdj_build = asyncio.run(client.get_build())
+    vdj_build = client.run("get_build")
     console.print(f"VirtualDJ script < get_build > => {vdj_build}")
 
     # test 1a
-    result1a = asyncio.run(client.get_browsed_title_artist())
+    result1a = client.run("get_browsed_title_artist")
     console.print(f"VirtualDJ script < get_browsed_title_artist > => {result1a}")
 
     # test 1b
-    result1b = asyncio.run(client.get_bpm('left'))
+    result1b = client.run("get_bpm", vdj_deck='left')
     console.print(f"VirtualDJ script < deck left get_bpm > => {result1b}")
 
     # test 2a
     vdj_script2a = "deck 1 play_pause & loop 4 & crossfader -5%"
-    result2a = asyncio.run(client.execute_vdj_script(vdj_script2a))
+    result2a = client.run("execute_vdj_script", vdj_script=vdj_script2a)
     console.print(f"VirtualDJ script < {vdj_script2a} > => {result2a}")
 
     # test 2b
-    result2b = asyncio.run(client.play_button('right'))
+    result2b = client.run("play_button", vdj_deck='right')
     console.print(f"VirtualDJ script < deck right play_button > => {result2b}")
+
+    # test 2c
+    result2c = client.run("play_pause")
+    console.print(f"VirtualDJ script < play_pause > => {result2c}")
 
 
 #------------------------------------------------------------------------------------------------------------------------------------
