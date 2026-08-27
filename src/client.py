d0e883471e8bf99d5eb27c42af39d783cf435cc9
djcel <16272026+DJCEL@users.noreply.github.com>
2026-08-27 07:20:40 +0200

    Update client.py

--- src/client.py
+++ src/client.py
@@ -15,6 +15,8 @@ from config import VDJ_PROCESS_NAME
 
 logger = logging.getLogger(__name__)
 
+#logging.basicConfig(filename='client.log', level=logging.INFO)
+
 #------------------------------------------------------------------------------------------------------------------------------------
 class VDJDeck:
     name : Literal['left', 'right', 'leftvideo', 'rightvideo', 'all', 'default', 'active', 'master']
