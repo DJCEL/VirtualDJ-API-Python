1877e26230118848f9ef1d1b2811a50140244f71
djcel <16272026+DJCEL@users.noreply.github.com>
2026-08-29 11:53:21 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,3 +8,4 @@ VDJ_NETWORK_CONTROL_DEBUG = False # default (bool): False
 # VirtualDJ - Process name in tasks manager
 VDJ_PROCESS_NAME = "virtualdj"
 VDJ_PROCESS_PATH_WINDOWS = r"C:\Program Files\VirtualDJ\virtualdj.exe"
+VDJ_PROCESS_PATH_MAC = r"/Applications/VirtualDJ.app/Contents/MacOS/VirtualDJ"
