3324846bffbdfd1b943f383e8d3b70d097395577
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-09-04 20:20:29 +0200

    update

--- src/virtualdj_client/history.py
+++ src/virtualdj_client/history.py
@@ -8,21 +8,21 @@ import platform
 __version__ = '1.0.0'
 
 class VirtualDJHistory():
-    TRACKLIST_NAME = "tracklist.txt"
+    TRACKLIST_FILENAME = "tracklist.txt"
+    FILES_EXTENSION = ".m3u"
 
-    def get_local_history_list(self) -> list[Path]:
+    def get_local_history_files(self):
         system = platform.system()
-
         if system == "Windows":
             local_appdata = os.getenv('LOCALAPPDATA')
             if local_appdata:
-                history_path = os.path.join(local_appdata, 'History', self.TRACKLIST_NAME)
-                if os.path.exists(history_path):
-                        self.read_tracklist_file(history_path)
+                 self.read_tracklist_file(local_appdata)
 
-    def read_tracklist_file(filepath:str):
-        with open(filepath,"r", encoding="utf-8") as file:
-            for line in file:
-                print(line)
+    def read_tracklist_file(self,local_appdata:str):
+        history_path = os.path.join(local_appdata, 'History', self.TRACKLIST_FILENAME)
+        if os.path.exists(history_path):
+            with open(history_path,"r", encoding="utf-8") as file:
+                for line in file:
+                    print(line)
          
 
