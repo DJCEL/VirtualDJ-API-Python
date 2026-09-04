919d6da13e6c61cc5452a1e52735dfe4e9911a30
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-09-04 20:09:30 +0200

    update

--- src/virtualdj_client/__init__.py
+++ src/virtualdj_client/__init__.py
@@ -1,9 +1,11 @@
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .client import VirtualDJClient, VDJError
 from .songs_database import VirtualDJSongsDatabase
+from .history import VirtualDJHistory
 
 from .client import __version__ as __client_version__
 from .songs_database import __version__ as __songs_database_version__
+from .history import __version__ as __history_version__
 
-__all__ = ["__version__", "VirtualDJClient", "VDJError", "VirtualDJSongsDatabase","__client_version__","__songs_database_version__"]
+__all__ = ["__version__", "VirtualDJClient", "VDJError", "VirtualDJSongsDatabase","VirtualDJHistory","__client_version__","__songs_database_version__","__history_version__"]
