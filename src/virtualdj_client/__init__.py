da287657f1c34358391b55f2c0ad688dfd35492f
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-09-08 01:36:58 +0200

    update

--- src/virtualdj_client/__init__.py
+++ src/virtualdj_client/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "1.1.2"
 
-from .client import VirtualDJClient
+from .client import VirtualDJClient, VDJDeck
 from .client_utils import VirtualDJClientUtils
 from .client_songs_database import VirtualDJSongsDatabase
 from .client_history_files import VirtualDJHistoryFiles
@@ -10,4 +10,4 @@ from .client_utils import __version__ as __client_utils_version__
 from .client_songs_database import __version__ as __client_songs_database_version__
 from .client_history_files import __version__ as __client_history_files_version__
 
-__all__ = ["__version__", "VirtualDJClient", "VirtualDJClientUtils", "VirtualDJSongsDatabase","VirtualDJHistoryFiles","__client_version__","__client_utils_version__","__client_songs_database_version__","__client_history_files_version__"]
+__all__ = ["__version__", "VirtualDJClient", "VDJDeck", "VirtualDJClientUtils", "VirtualDJSongsDatabase","VirtualDJHistoryFiles","__client_version__","__client_utils_version__","__client_songs_database_version__","__client_history_files_version__"]
