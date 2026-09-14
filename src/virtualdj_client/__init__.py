f0c2a573097e72d570c0ff8dbe9b9fc996c4d3c5
djcel <16272026+DJCEL@users.noreply.github.com>
2026-09-14 08:15:03 +0200

    Update __init__.py

--- src/virtualdj_client/__init__.py
+++ src/virtualdj_client/__init__.py
@@ -26,5 +26,5 @@ __all__ = [
     "__client_utils_version__",
     "__client_songs_database_version__",
     "__client_history_files_version__",
-    "__client_settings_version__",
+    "__client_settings_version__"
 ]
