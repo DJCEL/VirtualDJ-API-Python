539e986a61d0514a01a51c0ca91e80542b115fbe
djcel <16272026+DJCEL@users.noreply.github.com>
2026-09-17 06:25:30 +0200

    Update main_gui.py

--- src/main_gui.py
+++ src/main_gui.py
@@ -70,7 +70,7 @@ class VirtualDJMonitor(tk.Tk):
         
     #------------------------------------------------------------------------------------
     def _define_frame_get(self, parent):
-        frames_definition = [
+        frames_get_definition = [
             ("leftdecksong", "leftdecksong_frame", "Left Deck - Song"),
             ("leftdeckengine", "leftdeckengine_frame", "Left Deck - Engine"),
             ("rightdecksong", "rightdecksong_frame", "Right Deck - Song"),
@@ -82,7 +82,7 @@ class VirtualDJMonitor(tk.Tk):
 
         self.frames_get = {}
 
-        for row, (name,frame_id,frame_title) in enumerate(frames_definition):
+        for row, (name,frame_id,frame_title) in enumerate(frames_get_definition):
             frame = self._make_frame(parent, frame_title)
             setattr(self, frame_id, frame)
             self.frames_get[name] = frame
