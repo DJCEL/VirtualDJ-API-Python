5aab2685cfb6997f8cc234372e8d3f3548d4a23e
djcel <16272026+DJCEL@users.noreply.github.com>
2026-09-25 05:58:04 +0200

    Create client_logging.py

---
+++ src/virtualdj_client/client_logging.py
@@ -0,0 +1,43 @@
+import logging
+from typing import Literal
+
+from .client_config import VDJ_CLIENT_DEBUG
+
+LOG_FOLDER = './log'
+LOG_FILENAME = 'client.log'
+
+def configure_client_log(level: Literal["DEBUG","INFO","WARNING","ERROR","CRITICAL"] = "INFO") -> None:
+            filepath = f"{LOG_FOLDER}/{LOG_FILENAME}"
+            if not os.path.exists(LOG_FOLDER):
+                os.makedirs(LOG_FOLDER)
+            
+            FORMAT = '%(asctime)s - %(message)s'
+            handlers = list[logging.Handler] = []
+            
+            file_handler = logging.FileHander(filename=filepath)
+            file_handler.setLevel(level)
+            formatter = logging.Formatter(FORMAT)
+            file_handler.setFormatter(formatter)
+            handlers.append(file_handler)
+            
+            if not handlers:
+               handlers.append(logging.StreamHandler())
+
+            logging.basicConfig(filename=filepath, level=level, format=FORMAT)
+
+            #logging.basicConfig(level=level, format=FORMAT, handlers=handlers)
+    #------------------------------------------------------------------------------------
+    def get_client_log(name: str) -> logging.Logger:
+        return logging.getLogger(str)
+#------------------------------------------------------------------------------------
+    def save_client_log(level: str = "INFO", msg: str) -> None:
+        if VDJ_CLIENT_DEBUG == False:
+            return
+
+        logger = get_client_log(__name__)
+
+        if level == "INFO":
+            logger.info(msg)
+    #------------------------------------------------------------------------------------
+    def close_client_log():
+        logging.shutdown()
